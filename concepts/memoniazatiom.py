4754f934c6bf832235975910f1b967c5af75c221
devesh-bhushan <65954731+devesh-bhushan@users.noreply.github.com>
2020-06-05 01:02:12 +0530

    added program using memonic techique and decorator function

--- concepts/memoniazatiom.py
+++ concepts/memoniazatiom.py
@@ -1,21 +1,5 @@
 """
-lst = list([])
-
-
-for i in range(2):
-    suls = list([])
-    for j in range(2):
-        data = eval(input("Enter The Employee ID and Name"))
-        suls.append(data)
-    lst.append(suls)
-
-size=0
-lst1 = []
-lst =["james","anna","king","smith","kimberlee"]
-for i in lst:
-    size = len(i)
-    lst1.append(size)
-print(lst1)
+program using memonic techique and decorator function
 """
 def deco(fibo):
     d = {}
